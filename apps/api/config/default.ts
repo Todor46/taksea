f3157c562b4cf13a148771678693f8c6ade8fcfc
Bojan Bulatovic <bobu@frogne.dk>
2023-11-16 20:05:48 +0100

    Server initialization and create user endpoint

---
+++ apps/api/config/default.ts
@@ -0,0 +1,6 @@
+export default
+{
+    port: 3000,
+    dbUri:"mongodb://0.0.0.0:27017/Taksea1DB",
+    saltWorkFactor: 10
+}
---
+++ apps/api/src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import config from 'config';
+import routes from './routes';
+import connect from './utils/connect';
+
+const port = config.get<number>('port');
+const app = express();
+app.use(express.json());
+app.listen(port,async()=>{
+    console.log("App is running");
+    await connect();
+    routes(app);
+
+});
